4b89570c9d7430a290d9effea2ddb6eba942b541
Arpit <arpitdalal21@gmail.com>
2020-07-19 09:00:18 -0300

    Updates

---
+++ precache-manifest.90ec4a88a8d0f21ef17263157f5d15fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73d7391460af6507d0353810691b685",
+    "url": "/lms/index.html"
+  },
+  {
+    "revision": "7ec8c92117f3fe2b965f",
+    "url": "/lms/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7046abc0a5e713ba6511",
+    "url": "/lms/static/css/main.9b534601.chunk.css"
+  },
+  {
+    "revision": "7ec8c92117f3fe2b965f",
+    "url": "/lms/static/js/2.f3f8db55.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lms/static/js/2.f3f8db55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7046abc0a5e713ba6511",
+    "url": "/lms/static/js/main.556eb008.chunk.js"
+  },
+  {
+    "revision": "2f74356d45abe6eb10e2",
+    "url": "/lms/static/js/runtime-main.c02ea92c.js"
+  }
+]);
