661a86b7e19a3321238685facd125fc3b474e9d8
Arpit <arpitdalal21@gmail.com>
2020-07-29 23:18:25 -0300

    refactored the navigation of the app to use urls using react-router-dom library

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Navbar, Nav, Form, FormControl } from '../BootstrapWrap';
-import OutlineButton from '../Button/OutlineBtn';
+import { Navbar, Nav } from '../BootstrapWrap';
+import './Navbar.css';
 
-const NavbarWrap = ({ setIsAuthenticated, setCurrentPage }) => {
+const NavbarWrap = ({ setIsAuthenticated }) => {
   return (
     <React.Fragment>
       <Navbar bg='primary' variant='dark'>
         <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
         <Nav className='mr-auto'>
-          <Nav.Link href='#home' onClick={() => setCurrentPage(1)}>
-            Home
-          </Nav.Link>
-          <Nav.Link href='#courses' onClick={() => setCurrentPage(2)}>
-            Courses
-          </Nav.Link>
-          <Nav.Link href='#students' onClick={() => setCurrentPage(3)}>
-            Students
-          </Nav.Link>
+          <Link to='/'>Home</Link>
+          <Link to='/course'>Course</Link>
+          <Link to='/student'>Student</Link>
         </Nav>
-        <Form inline>
-          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-          <OutlineButton variant='outline-light' text='Search' />
-        </Form>
         <Nav.Link
           href='#'
           style={{ color: 'white' }}
--- src/containers/Admin/Home.js
+++ src/containers/Admin/Home.js
@@ -1,30 +1,19 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import NavbarWrap from '../../components/Navbar/Navbar';
 import Dashboard from './Dashboard';
 import Student from './Student/Student';
 import Course from './Course/Course';
 
 const Home = ({ setIsAuthenticated }) => {
-  const [ currentPage, setCurrentPage ] = useState(1);
-
-  const getPage = () => {
-    switch (currentPage) {
-      case 1:
-        return <Dashboard />;
-      case 2:
-        return <Course />;
-      case 3:
-        return <Student />;
-      default:
-        return <Dashboard />;
-    }
-  };
-
   return (
     <React.Fragment>
-      <NavbarWrap setIsAuthenticated={setIsAuthenticated} setCurrentPage={setCurrentPage} />
-      {getPage()}
+      <NavbarWrap setIsAuthenticated={setIsAuthenticated} />
+      <Switch>
+        <Route exact path='/' component={Dashboard} />
+        <Route path='/student' component={Student} />
+        <Route path='/course' component={Course} />
+      </Switch>
     </React.Fragment>
   );
 };
