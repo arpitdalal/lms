47a25eb086713720486eaac71f6ea2d2bf149042
Arpit <arpitdalal21@gmail.com>
2020-07-29 23:19:14 -0300

    Updates

---
+++ precache-manifest.49f18009c61cca064fa5e453c0059d90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdba8dcefeb1938d8d73c2bc0bbc964f",
+    "url": "/lms/index.html"
+  },
+  {
+    "revision": "85b671fd291d12131c58",
+    "url": "/lms/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e9d525ec6e2b1475235",
+    "url": "/lms/static/css/main.4a4fd952.chunk.css"
+  },
+  {
+    "revision": "85b671fd291d12131c58",
+    "url": "/lms/static/js/2.37617d15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lms/static/js/2.37617d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9d525ec6e2b1475235",
+    "url": "/lms/static/js/main.5d167fd1.chunk.js"
+  },
+  {
+    "revision": "2f74356d45abe6eb10e2",
+    "url": "/lms/static/js/runtime-main.c02ea92c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lms/precache-manifest.6e84bc93c805974ff41dcabe46d17602.js"
+  "/lms/precache-manifest.49f18009c61cca064fa5e453c0059d90.js"
 );
 
 self.addEventListener('message', (event) => {
